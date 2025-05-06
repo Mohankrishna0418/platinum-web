f3658876ab183418160acb9010694c2cfc2ac2e0
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-05-06 14:32:51 +0530

    Update @tanstack/react-query to version 5.75.2
    - Updated dependency in package.json from @tanstack/react-query 4.36.1 to 5.75.2.
    - Updated pnpm-lock.yaml to reflect the new version and its dependencies.

---
+++ app/feed/components/create-post/CreatePost.tsx
@@ -0,0 +1,137 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+import { Textarea } from "@/components/ui/textarea";
+import { serverUrl } from "@/lib/environment";
+import { useForm } from "@tanstack/react-form";
+import { useMutation } from "@tanstack/react-query";
+import { PlusIcon } from "lucide-react";
+import React, { useState } from "react";
+
+export const CreatePost = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const createPostMutation = useMutation({
+    mutationFn: async (parameters: { text: string }) => {
+      const response = await fetch(`${serverUrl}/posts`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({
+          text: parameters.text,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+    },
+    onSuccess: () => {
+      setIsOpen(false);
+      reset()      
+    },
+  });
+
+  const { Field, Subscribe, handleSubmit, reset } = useForm({
+    defaultValues: {
+      text: "",
+    },
+    onSubmit: ({ value }) => {
+      console.log("OnSubmit");
+      const { text } = value;
+
+      createPostMutation.mutate({ text });
+    },
+  });
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button>
+          <PlusIcon />
+          Create Post
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>What&apos;s is on your mind?</DialogTitle>
+          <DialogDescription>
+            Politics, tech, climate or anything else...
+          </DialogDescription>
+        </DialogHeader>
+        <form
+          onSubmit={(e) => {
+            console.log("form.onSubmit");
+            e.preventDefault();
+            e.stopPropagation();
+            handleSubmit(e);
+          }}
+          className="flex flex-col items-stretch gap-4"
+        >
+          <Field
+            name="text"
+            validators={{
+              onChange: ({ value }) => {
+                if (value.length === 0) {
+                  return "Empty string";
+                }
+              },
+            }}
+          >
+            {(field) => {
+              return (
+                <Textarea
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  placeholder="Type your post here ..."
+                  className="resize-none h-32"
+                />
+              );
+            }}
+          </Field>
+
+          <Subscribe
+            selector={(state) => [
+              state.canSubmit,
+              state.isSubmitting,
+              state.isPristine,
+            ]}
+          >
+            {([canSubmit, isSubmitting, isPristine]) => {
+              return (
+                <Button
+                  disabled={
+                    !canSubmit ||
+                    isSubmitting ||
+                    isPristine ||
+                    createPostMutation.isPending
+                  }
+                  type="submit"
+                >
+                  {createPostMutation.isPending && <Spinner />}
+                  Create
+                </Button>
+              );
+            }}
+          </Subscribe>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreatePost;
